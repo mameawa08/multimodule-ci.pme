a60e8d06ee21bf3b8b43bdd13d9bf981a1a2b7d9
mameawa08 <mameawa08@gmail.com>
2021-10-28 00:54:26 +0000

    referentiel

---
+++ administration/src/main/java/com/administration/repository/QuestionRepository.java
@@ -0,0 +1,26 @@
+package com.administration.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.administration.model.Question;
+
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+	
+	@Query("SELECT DISTINCT q FROM Question q WHERE q.actif = 1 ORDER BY q.id ASC")
+	List<Question> findAllActif();
+	
+	@Query("SELECT DISTINCT q FROM Question q WHERE q.actif = 1 AND q.parametre IS NULL ORDER BY q.id ASC")
+	List<Question> findQuestionEligible();
+	
+	@Query("SELECT DISTINCT q FROM Question q WHERE q.actif = 1 AND q.parametre IS NOT NULL ORDER BY q.id ASC")
+	List<Question> findQuestionQualitative();
+	
+	@Query("SELECT DISTINCT q FROM Question q WHERE q.actif = 1 AND q.parametre IS NOT NULL AND q.parametre.id=:idParametre ORDER BY q.id ASC")
+	List<Question> findQuestionByParametre(@Param("idParametre") Long idParametre);
+
+}
---
+++ administration/src/main/java/com/administration/repository/ReponseRepository.java
@@ -0,0 +1,20 @@
+package com.administration.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.administration.model.ReponseQualitative;
+
+
+public interface ReponseRepository extends JpaRepository<ReponseQualitative, Long> {
+	
+	@Query("SELECT DISTINCT r FROM ReponseQualitative r WHERE r.actif = 1 ORDER BY r.id ASC")
+	List<ReponseQualitative> findAllActif();
+	
+	@Query("SELECT DISTINCT r FROM ReponseQualitative r WHERE r.actif = 1 AND r.question IS NOT NULL AND r.question.id=:idQuestion ORDER BY r.id ASC")
+	List<ReponseQualitative> findReponseByQuestion(@Param("idQuestion") Long idQuestion);
+
+}
