57cf315a0b65073f3e22308e73ffd1a0a3fc6741
msall1006 <mohamaddou.sall@awaconsulting.com>
2021-10-28 10:22:23 +0000

    Habilitation :: controller base endpoint

--- administration/src/main/java/com/administration/controller/HabilitationController.java
+++ administration/src/main/java/com/administration/controller/HabilitationController.java
@@ -9,10 +9,12 @@ import com.administration.service.IHabilitationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth/habilitations")
 public class HabilitationController {
 	
 	@Autowired
