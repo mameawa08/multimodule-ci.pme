c92f80506ed1168d44eadbcd6893d1de8511361d
msall1006 <mohamaddou.sall@awaconsulting.com>
2021-11-04 22:47:20 +0000

    Config :: oauth2

---
+++ administration/src/main/java/com/administration/config/oauth/server/AuthorizationServerConfig.java
@@ -0,0 +1,127 @@
+package com.administration.config.oauth.server;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.util.Date;
+
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+//    @Value("${ecourrier.app.jwtSecret}")
+//    private String jwtSecret;
+
+    @Value("${app.oauth.jwt.keystore-location}")
+    private String keyStorePath;
+
+    @Value("${app.oauth.jwt.keystore-password}")
+    private String keyStorePassword;
+
+    @Value("${app.oauth.jwt.key-alias}")
+    private String keyAlias;
+
+    @Value("${app.oauth.jwt.private-key-passphrase}")
+    private String privateKeyPassphrase;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private DataSource dataSource;
+
+//
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        JdbcClientDetailsService jdbcClientDetailsService = new JdbcClientDetailsService(dataSource);
+        clients.withClientDetails(jdbcClientDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
+                .authenticationManager(authenticationManager)
+//                .userDetailsService(userDetailsService)
+        ;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        JdbcTokenStore store = new JdbcTokenStore(dataSource);
+//        TokenStore store = new CustomTokenStore(accessTokenRepository, refreshTokenRepository);
+//        TokenStore store = new CustomJdbcTokenStore(dataSource);
+//        return new JwtTokenStore(accessTokenConverter());
+        return store;
+
+    }
+
+//    @Bean
+//    public JwtAccessTokenConverter accessTokenConverter() {
+////        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//        JwtAccessTokenConverter converter = new CustomTokenEnhancer();
+//        converter.setSigningKey(jwtSecret);
+//        return converter;
+//    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+
+        final KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(
+                new ClassPathResource(keyStorePath), keyStorePassword.toCharArray());
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair(keyAlias));
+        return converter;
+    }
+
+//    @Bean
+//    @Primary
+//    public DefaultTokenServices tokenServices() {
+//        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+//        defaultTokenServices.setTokenStore(tokenStore());
+//        defaultTokenServices.setSupportRefreshToken(true);
+//        defaultTokenServices.setAccessTokenValiditySeconds(3600);
+//        defaultTokenServices.setTokenEnhancer(accessTokenConverter());
+//        defaultTokenServices.setAuthenticationManager(authenticationManager);
+//        return defaultTokenServices;
+//    }
+
+//    @Scheduled(fixedRate = 1800000)
+//    public void purgeOldTokens() {
+//        JdbcTemplate jdbcTemplate;
+//        jdbcTemplate = new JdbcTemplate(dataSource);
+//        Date now = new Date();
+//        jdbcTemplate.update("delete from oauth_access_token where expiration <?", now);
+//    }
+}
