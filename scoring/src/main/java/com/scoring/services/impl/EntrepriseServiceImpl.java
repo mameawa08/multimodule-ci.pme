77ba1b2184ebd33313f78d261add94c848dc446b
msall1006 <mohamaddou.sall@awaconsulting.com>
2021-11-09 21:06:59 +0000

    Entreprise :: add

--- scoring/src/main/java/com/scoring/services/impl/EntrepriseServiceImpl.java
+++ scoring/src/main/java/com/scoring/services/impl/EntrepriseServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.scoring.dto.EntrepriseDTO;
 import com.scoring.exceptions.EntrepriseException;
 import com.scoring.mapping.DTOFactory;
+import com.scoring.mapping.ModelFactory;
+import com.scoring.mapping.PayloadToDTO;
 import com.scoring.models.Entreprise;
 import com.scoring.payloads.EntreprisePayload;
 import com.scoring.repositories.EntrepriseRepository;
@@ -18,9 +20,15 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 
 	@Autowired
 	private EntrepriseRepository entrepriseRepository;
-	
+
 	@Autowired
 	private DTOFactory dtoFactory;
+
+	@Autowired
+	private PayloadToDTO payloadToDTO;
+
+	@Autowired
+	private ModelFactory modelFactory;
 
 
 	@Override
@@ -37,7 +45,55 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 
 	@Override
 	public EntrepriseDTO createEntreprise(EntreprisePayload payload) throws EntrepriseException{
-		return null;
+		EntrepriseDTO entreprise = null;
+		if(payload.getId() != null){
+			entreprise = getEntreprise(payload.getId());
+		}
+		else{
+			entreprise = new EntrepriseDTO();
+		}
+		if(entreprise.getRaisonSociale() != null && entreprise.getRaisonSociale().equals(""))
+			throw new EntrepriseException("La raison sociale est obligatoire.");
+
+		if(entreprise.getSecteur() != null && entreprise.getSecteur().equals(""))
+			throw new EntrepriseException("Le secteur est obligatoire.");
+
+		if(entreprise.getDescription() != null && entreprise.getDescription().equals(""))
+			throw new EntrepriseException("La description est obligatoire.");
+
+		if(entreprise.getRegime() != null && entreprise.getRegime().equals(""))
+			throw new EntrepriseException("Le regime est obligatoire.");
+
+		if(entreprise.getAdresse() != null && entreprise.getAdresse().equals(""))
+			throw new EntrepriseException("L'adresse est obligatoire.");
+
+		if(entreprise.getSiteWeb() != null && entreprise.getSiteWeb().equals(""))
+			throw new EntrepriseException("Le site web est obligatoire.");
+
+		if(entreprise.getLogo() != null && entreprise.getLogo().equals(""))
+			throw new EntrepriseException("Le logo est obligatoire.");
+
+		if(entreprise.getFormeJur() != null && entreprise.getFormeJur().equals(""))
+			throw new EntrepriseException("La forme juridique est obligatoire.");
+
+		if(entreprise.getAnnee() == 0)
+			throw new EntrepriseException("L'annee de creation est obligatoire.");
+
+		if(entreprise.getCapital() != null && entreprise.getCapital().equals(0L))
+			throw new EntrepriseException("Le capital est obligatoire.");
+
+		entreprise = payloadToDTO.createEntreprise(payload);
+		entreprise.setActif(true);
+
+		try {
+			Entreprise model = modelFactory.createEntreprise(entreprise);
+			entrepriseRepository.save(model);
+			entreprise.setId(model.getId());
+		}
+		catch (Exception e){
+			throw new EntrepriseException(e.getMessage(), e);
+		}
+		return entreprise;
 	}
 
 	@Override
