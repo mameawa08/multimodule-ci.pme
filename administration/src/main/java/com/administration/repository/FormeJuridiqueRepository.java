1683f88d08b6dbfbcc1900304f6beda8cb2bed32
mameawa08 <mameawa08@gmail.com>
2021-11-16 11:04:41 +0000

    ref techniques

---
+++ administration/src/main/java/com/administration/repository/FormeJuridiqueRepository.java
@@ -0,0 +1,9 @@
+package com.administration.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.administration.model.FormeJuridique;
+
+public interface FormeJuridiqueRepository extends JpaRepository<FormeJuridique, Long> {
+	
+}
---
+++ administration/src/main/java/com/administration/repository/SecteurActiviteRepository.java
@@ -0,0 +1,9 @@
+package com.administration.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.administration.model.SecteurActivite;
+
+public interface SecteurActiviteRepository extends JpaRepository<SecteurActivite, Long> {
+	
+}
