4520cedd29d6bc2bdcddafe79bca8e68803d95de
msall1006 <mohamaddou.sall@awaconsulting.com>
2021-11-22 10:02:13 +0000

    init sql && oauth  secret

--- administration/src/main/resources/sql/init-admin.sql
+++ administration/src/main/resources/sql/init-admin.sql
@@ -9,5 +9,5 @@ insert into profils(profil_id, actif, code, libelle) VALUES (4, 1, 'ROLE_EXP_PME
 INSERT INTO habilitations(id, code, libelle) VALUES (1, 'VOIR_PME', 'Voir liste des PMEs');
 
 --UTILISATEURS
-insert into users(user_id, actif, email, identifiant, mot_de_passe, nom, prenom, profil_id, confirme, mdpamodifier)
+insert into users(user_id, actif, email, username, mot_de_passe, nom, prenom, profil_id, confirme, mdpamodifier)
 values (1, 1, 'admin@um.com', 'admin', '$2a$12$fEw1QzwhsSWmp6J9jzGwmu65OcCl6QAwDZnTo8PmeWt1zsyXnqSB.', 'admin', 'admin', 0, 1, 0);
