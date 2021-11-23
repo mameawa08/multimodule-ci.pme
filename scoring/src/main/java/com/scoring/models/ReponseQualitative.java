33cef70f8cf59f4ba136d392e306165f48e07624
mameawa08 <mameawa08@gmail.com>
2021-11-23 09:32:06 +0000

    com

---
+++ scoring/src/main/java/com/scoring/models/ReponseQualitative.java
@@ -0,0 +1,53 @@
+package com.scoring.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author agileway
+ */
+
+@Setter
+@Getter
+@Entity
+@Table(name = "reponse_qualitative")
+public class ReponseQualitative implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5297205263579630551L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
+    private Long id;
+	
+	@Column
+	private String code;
+
+    @Column
+    private String libelle;
+    
+    @Column
+    private int score;
+    
+    @ManyToOne
+  	@JoinColumn(name = "id_question")
+    private Question  question;
+    
+    @Column(name = "actif")
+	private int	actif;
+
+}
