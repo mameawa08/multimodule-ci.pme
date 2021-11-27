96f0c1190f7986a43764a57b9456bf9889531b32
mameawa08 <mameawa08@gmail.com>
2021-11-27 15:56:15 +0000

    corrections sur ratios et score financier

--- administration/src/main/java/com/administration/repository/CalibrageRepository.java
+++ administration/src/main/java/com/administration/repository/CalibrageRepository.java
@@ -18,9 +18,11 @@ public interface CalibrageRepository extends JpaRepository<Calibrage, Long> {
 	@Query("SELECT DISTINCT c FROM Calibrage c WHERE c.actif = 1 AND c.ratio IS NOT NULL AND c.ratio.id=:idRatio ORDER BY c.id ASC")
 	List<Calibrage> findCalibrageByRatio(@Param("idRatio") Long idRatio);
 	
-	@Query("SELECT DISTINCT c FROM Calibrage c WHERE c.actif = 1 AND c.ratio IS NOT NULL AND c.ratio.id=:idRatio "
-			+ "AND c.min IS NOT NULL AND :valeurRatio > c.min "
-			+ "AND c.max IS NOT NULL AND :valeurRatio <= c.max")
+	@Query("SELECT DISTINCT c FROM Calibrage c WHERE c.actif = 1 AND c.ratio IS NOT NULL AND c.ratio.id=:idRatio AND "
+			+ "((c.min IS NOT NULL AND :valeurRatio > c.min "
+			+ "AND c.max IS NOT NULL AND :valeurRatio <= c.max) "
+			+ "OR (c.min IS NULL AND :valeurRatio <= c.max) "
+			+ "OR (c.max IS NULL AND :valeurRatio > c.min))")
 	Calibrage findCalibrageByRatioAndValeurCalcule(@Param("idRatio") Long idRatio, @Param("valeurRatio") BigDecimal valeurRatio);
 
 }
