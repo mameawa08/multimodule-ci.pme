64ce5c509642fb61498c447abc4b64d2b1c11402
msall1006 <mohamaddou.sall@awaconsulting.com>
2021-11-29 09:04:04 +0000

    Generate report (ok)

--- scoring/src/main/java/com/scoring/services/IReportPrinterService.java
+++ scoring/src/main/java/com/scoring/services/IReportPrinterService.java
@@ -2,14 +2,17 @@ package com.scoring.services;
 
 import com.scoring.exceptions.ReportPrinterException;
 
+import java.util.List;
 import java.util.Map;
 
 public interface IReportPrinterService {
+
 
     enum TypeFile{
         PDF,
         XLSX
     }
 
-    public String print(String jasperFile, String filename, TypeFile type, Map params) throws ReportPrinterException;
+//    public String print(String jasperFile, String filename, TypeFile type, Map params) throws ReportPrinterException;
+    String print(String jasperFile, String filename, TypeFile type, Map params, List data) throws ReportPrinterException;
 }
